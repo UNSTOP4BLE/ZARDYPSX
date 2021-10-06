a80bbf56be12a21bda9db4bdb06ab56ae4425a97
UNSTOP4BLE <bruno.juraski1@gmail.com>
2021-10-06 19:09:16 +0200

    Initial commit

---
+++ src/stagedef_disc1.h
@@ -0,0 +1,82 @@
+	{ //StageId_1_1 (Bopeebo)
+		//Characters
+		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
+		{Char_Zardy_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
+		{Char_GF_New,     FIXED_DEC(0,1),    FIXED_DEC(0,1)},
+		
+		//Stage background
+		Back_Week4_New,
+		
+		//Song info
+		{FIXED_DEC(1,1),FIXED_DEC(29,10),FIXED_DEC(13,10)},
+		1, 1,
+		XA_Bopeebo, 0,
+		
+		StageId_1_2, 0
+	},
+	{ //StageId_1_2 (Fresh)
+		//Characters
+		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
+		{Char_Zardy_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
+		{Char_GF_New,     FIXED_DEC(0,1),    FIXED_DEC(0,1)},
+		
+		//Stage background
+		Back_Week4_New,
+		
+		//Song info
+		{FIXED_DEC(1,1),FIXED_DEC(35,10),FIXED_DEC(18,10)},
+		1, 2,
+		XA_Fresh, 2,
+		
+		StageId_1_3, 0
+	},
+	{ //StageId_1_3 (Dadbattle)
+		//Characters
+		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
+		{Char_Zardy_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
+		{Char_GF_New,     FIXED_DEC(0,1),    FIXED_DEC(0,1)},
+		
+		//Stage background
+		Back_Week4_New,
+		
+		//Song info
+		{FIXED_DEC(13,10),FIXED_DEC(15,10),FIXED_DEC(23,10)},
+		1, 3,
+		XA_Dadbattle, 0,
+		
+		StageId_1_3, 0
+	},
+	{ //StageId_1_4 (Tutorial)
+		//Characters
+		{Char_BF_New,   FIXED_DEC(60,1),  FIXED_DEC(100,1)},
+		{Char_Dad_New,   FIXED_DEC(0,1),  FIXED_DEC(-15,1)}, //TODO
+		{Char_GF_New,    FIXED_DEC(0,1),    FIXED_DEC(0,1)},
+		
+		//Stage background
+		Back_Week1_New,
+		
+		//Song info
+		{FIXED_DEC(1,1),FIXED_DEC(1,1),FIXED_DEC(1,1)},
+		1, 4,
+		XA_Tutorial, 2,
+		
+		StageId_1_4, 0
+	},
+	
+	
+	{ //StageId_2_1 (Spookeez)
+		//Characters
+		{Char_BF_New,      FIXED_DEC(56,1),   FIXED_DEC(85,1)},
+		{Char_XmasP_New,  FIXED_DEC(-90,1),   FIXED_DEC(85,1)},
+		{Char_GF_New,       FIXED_DEC(0,1),  FIXED_DEC(-1500,1)},
+		
+		//Stage background
+		Back_Week2_New,
+		
+		//Song info
+		{FIXED_DEC(1,1),FIXED_DEC(27,10),FIXED_DEC(24,10)},
+		2, 1,
+		XA_Spookeez, 0,
+		
+		StageId_2_1, 0
+	},
